a2d568a231d29d580dad456190ccbb33a42b5473
cathcharles108 <catherinecharles108@gmail.com>
2022-05-04 17:03:37 -0500

    working version

--- inputmaker.py
+++ inputmaker.py
@@ -4,17 +4,37 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import random
+import math
+
+def distance(x,y):
+    return math.sqrt((x-200)**2 + (y-200)**2)
 
 def makeinput(N):
     x, y, u, o, c, t = [], [], [], [], [], []
     for i in range(N):
-        x.append(random.randint(0, 401))
-        y.append(random.randint(0, 401))
-        u.append(random.randint(1, 27201))
-        o.append(random.randint(0, 14401))
+        while True:
+            x1 = random.randint(0, 401)
+            y1 = random.randint(0, 401)
+            u1 = random.randint(1, 27201)
+            o1 = random.randint(0, 1441)
+            c1 = random.randint(o1,1441)
+            test = int(1441-c1-distance(x1,y1))
+            if (test >= 1):
+                t1 = random.randint(1, test)
+                x.append(x1)
+                y.append(y1)
+                u.append(u1)
+                o.append(o1)
+                c.append(c1)
+                t.append(t1)
+                break
+            else:
+                continue
+        print(f"{x[i]} {y[i]} {o[i]} {c[i]} {u[i]} {t[i]}")
 
 def readinput():
     N = int(input())
+    print(N)
     return N
 
 def main():
@@ -23,4 +43,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
