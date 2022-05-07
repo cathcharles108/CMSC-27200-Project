555ca0d6d169e0d71e926aa17ad7ff4d15fbe52e
Catherine Charles <60662989+cathcharles108@users.noreply.github.com>
2022-05-07 00:23:09 +0000

    confusion

--- inputmaker.py
+++ inputmaker.py
@@ -7,8 +7,8 @@ import random
 import math
 import numpy as np
 
-def distance(x,y):
-    return math.sqrt((x-200)**2 + (y-200)**2)
+def distance(x1,y1,x2,y2):
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 def makeinput(N):
     x, y, u, o, c, t = [], [], [], [], [], []
@@ -19,29 +19,37 @@ def makeinput(N):
             u1 = random.randint(1, 27200)
             o1 = random.randint(0, 1440)
             c1 = random.randint(o1,1440)
-            test = int(1440 - c1 - distance(x1, y1))
+            test = int(1440 - c1 - distance(x1, y1, 200, 200))
             if test >= 1:
                     m = 1440 - o1
-                    t1 = random.randint(c1-o1+1, m)
+                    t1 = random.randint(c1-o1, m)
                     if 0 <= i <= 2**(int(math.log(N,2)) - 3) - 1:
-                        if o1+t1 - c1 + 1 <= 30:
+                        if o1+t1 - c1  <= 50:
                             continue
-                        u_new = u1 + 500
-                        ulist = [int(np.ceil(u_new/3)) for _ in range(3)]
+                        unew = u1 + 500
+                        d1 = random.randint(1, unew + 500)
+                        d2 = random.randint(d1, unew + 500)
+                        ulist = [d1, d2 - d1, unew + 500 - d2]
                         nested = [[] for _ in range(3)]
-                        nested[0].append(random.randint(0, 400))
-                        nested[0].append(random.randint(0, 400))
-                        nested[0].append(random.randint(c1, o1+t1))
-                        nested[0].append(random.randint(nested[0][-1], o1+t1))
+                        l = o1+t1 - c1 + 1
+                        nested[0].append(pointwithin(x1, y1, l/12)[0])
+                        nested[0].append(pointwithin(x1, y1, l/12)[1])
+                        nested[0].append(random.randint(c1+int(l/12), c1+np.floor(3*l/12)))
+                        nested[0].append(random.randint(c1+np.floor(3*l/12) - nested[0][-1], c1 + int(3*l/12)))
                         nested[0].append(ulist[0])
-                        nested[0].append(random.randint(1, nested[0][3] - nested[0][2]))
-                        for j in range(2):
-                            nested[j+1].append(random.randint(0, 400))
-                            nested[j+1].append(random.randint(0, 400))
-                            nested[j+1].append(random.randint(nested[j][3], o1+t1))
-                            nested[j+1].append(random.randint(nested[j+1][-1], o1+t1))
-                            nested[j+1].append(ulist[j+1])
-                            nested[j+1].append(random.randint(1, nested[j+1][3] - nested[j+1][2]))
+                        nested[0].append(random.randint(1, nested[0][3] - nested[0][2] + 1))
+                        nested[1].append(pointwithin(nested[0][0], nested[0][1], l/12)[0])
+                        nested[1].append(pointwithin(nested[0][0], nested[0][1], l/12)[1])
+                        nested[1].append(random.randint(c1+int(4*l/12), c1+np.floor(6*l/12)))
+                        nested[1].append(random.randint(c1+np.floor(6*l/12) - nested[1][-1], c1+np.floor(6*l/12)))
+                        nested[1].append(ulist[1])
+                        nested[1].append(random.randint(1, nested[1][3] - nested[1][2] + 1))
+                        nested[2].append(pointwithin(nested[1][0], nested[1][1], l/12)[0])
+                        nested[2].append(pointwithin(nested[1][0], nested[1][1], l/12)[1])
+                        nested[2].append(random.randint(c1+int(7*l/12), c1+int(9*l/12)))
+                        nested[2].append(random.randint(c1+int(9*l/12) - nested[2][-1], c1 +int(9*l/12)))
+                        nested[2].append(ulist[2])
+                        nested[2].append(random.randint(1, nested[2][3] - nested[2][2] + 1))
                         for k in range(3):
                             x.append(nested[k][0])
                             y.append(nested[k][1])
@@ -58,6 +66,9 @@ def makeinput(N):
                     break
             else:
                 continue
+    randomlist = list(range(1, N+1))
+    random.shuffle(randomlist)
+    for i in randomlist:
         print(f"{x[i]} {y[i]} {o[i]} {c[i]} {u[i]} {t[i]}")
 
     # for i in range(N):
@@ -84,6 +95,13 @@ def makeinput(N):
     #         print(f"{x[i]} {y[i]} {o[i]} {c[i]} {u[i]} {t[i]}", file=input_file)
     #         input_file.close()
 
+def pointwithin(x, y, d):
+    while True:
+        xnew = random.randint(0, 400)
+        ynew = random.randint(0, 400)
+        if distance(x, y, xnew, ynew) < int(d):
+            return [xnew, ynew]
+          
 def readinput():
     N = int(input())
     # with open("inputs/large3.in", "w") as input_file:
